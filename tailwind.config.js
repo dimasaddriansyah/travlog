0ea63c27deaff2069e776512c201a5d7fb4cfce3
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2023-09-18 15:38:23 +0700

    Configuration Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "purple" : "#5D50C6",
+        "pink" : "#F85E9F",
+        "orange" : "#FF5722",
+        "grey" : "#191825",
+      }
+    },
+  },
+  plugins: [],
+}
+
