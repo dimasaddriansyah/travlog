6d09af01cc64a20bc587d092be81129693261db9
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2023-09-27 16:43:01 +0700

    Add Box Shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         "purple" : "#5D50C6",
         "pink" : "#F85E9F",
         "orange" : "#FF5722",
-        "grey" : "#191825",
+        "grey" : "#191825"
       },
       dropShadow:{
         "shadow-sm" : [
@@ -18,20 +18,30 @@ module.exports = {
           "0px 308px 185px rgba(0, 0, 0, 0.04)",
           "0px 137px 137px rgba(0, 0, 0, 0.06)",
           "0px 34px 75px rgba(0, 0, 0, 0.07)",
-          "0px 0px 0px rgba(0, 0, 0, 0.07)",
+          "0px 0px 0px rgba(0, 0, 0, 0.07)"
         ],
         "shadow-lg": [
           "0px 81px 32px rgba(0, 0, 0, 0.01)",
           "0px 45px 27px rgba(0, 0, 0, 0.05)",
           "0px 20px 20px rgba(0, 0, 0, 0.09)",
           "0px 5px 11px rgba(0, 0, 0, 0.1)",
-          "0px 0px 0px rgba(0, 0, 0, 0.1)",
+          "0px 0px 0px rgba(0, 0, 0, 0.1)"
         ],
         "shadow-xl": [
           "0px 41px 89px rgba(0, 0, 0, 0.1)",
-          "0px 0px 0px rgba(0, 0, 0, 0.1)",
-        ]
-      }
+          "0px 0px 0px rgba(0, 0, 0, 0.1)"
+        ],
+      },
+      boxShadow:{
+        "shadow-xl": [
+          "0px 0px 0px 0px rgba(0, 0, 0, 0.05)",
+           "0px 32px 71px 0px rgba(0, 0, 0, 0.05)",
+           "0px 128px 128px 0px rgba(0, 0, 0, 0.04)", 
+           "0px 288px 173px 0px rgba(0, 0, 0, 0.03)", 
+           "0px 513px 205px 0px rgba(0, 0, 0, 0.01)", 
+           "0px 801px 224px 0px rgba(0, 0, 0, 0.00)"
+         ],
+      },
     },
   },
   plugins: [],
